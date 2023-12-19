b228a955a78f5d577133eb0325f4a04f36f3dbb4
denalichin <denalichin@gmail.com>
2023-12-18 22:09:41 -0800

    login button added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  /*global google*/ 
+  // ^ above comment prevents lintor from marking google as undefined (it's defined in index.html)
+  
+  function handleCallbackResponse(response){
+    console.log("Signin Success, Encoded JWT ID token: " + response.credential);
+  }
+
+  useEffect(()=> {
+    google.accounts.id.initialize({
+      client_id: "226902442258-hbu4p3v9q6gkhtnkpdbj8522uo4fr1l1.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    })
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large"}
+    );
+
+  }, []); //if anything in array is modified, it reloads the page
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
     </div>
   );
 }
